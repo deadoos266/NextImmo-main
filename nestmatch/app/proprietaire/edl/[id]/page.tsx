880dd37b68e5ab66628c6247bb8bd60bb1b19713
deadoos266 <paul.david.56890@gmail.com>
2026-04-16 17:30:19 +0200

    fix: remove unused setPieceToAdd reference in EDL page

--- nestmatch/app/proprietaire/edl/[id]/page.tsx
+++ nestmatch/app/proprietaire/edl/[id]/page.tsx
@@ -334,7 +334,6 @@ export default function EdlPage() {
     const nom = customName || typeName
     const t = detectType(nom)
     setPieces(prev => [...prev, { nom, type: t, elements: makeElements(t), photos: [] }])
-    setPieceToAdd("")
     setCustomPieceName("")
   }
 
